d5c514d792b11ae22ef8eae9d0715f77da7e50c0
Milos Andrejic <milosandrejic1993@gmail.com>
2023-10-19 18:34:30 +0200

    routing, page structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,39 @@
 import {createRoot} from "react-dom/client";
 import React from "react";
 
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import Login from "pages/login";
+import PageOne from "pages/pageOne";
+import PageTwo from "pages/pageTwo";
+import PageTree from "pages/pageTree";
+
+const router = createBrowserRouter([
+    {
+        path: "*",
+        element: <Login />
+    },
+    {
+        path: "/page-one",
+        element: <PageOne />
+    },
+    {
+        path: "/page-two",
+        element: <PageTwo />
+    },
+    {
+        path: "/page-tree",
+        element: <PageTree />
+    },
+]);
+
 const container = document.getElementById("app");
 
 if (container) {
     const root = createRoot(container);
 
-    root.render(<div>hello</div>);
+    root.render(<RouterProvider router={router} />);
 }
---
+++ src/pages/pageTree/index.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <div>page tree</div>;
